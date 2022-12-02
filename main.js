13f07e0a9e275b096eb37dc246d92eb8bf6435ca
Julien Peeters <julienpeeters.personal@gmail.com>
2022-12-02 22:32:50 +0100

    ''

--- main.js
+++ main.js
@@ -3,7 +3,3 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
    maxZoom: 19,
    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-// var marker = L.marker([51.23009, 4.41616]).addTo(map);
-// var bounds = [[51.23041, 4.4155], [51.22991, 4.41675]]
-// L.rectangle(bounds, { color: "#ff7800", weight: 1 }).addTo(map);
-// marker.bindPopup('AP-Hogeschool<br>Ellermanstraat 33').openPopup()
