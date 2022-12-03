f350e9c2dd480d6e51358cc5bf01c917177940db
Julien Peeters <julienpeeters.personal@gmail.com>
2022-12-03 20:49:47 +0100

    ''

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-let map = L.map('map').setView([51.23009, 4.41616], 17);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   maxZoom: 19,
-   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+const nav = document.querySelector(".nav");
+nav.innerHTML += `
+<a href="../index.html" id="linkHome">
+	<img src = "../assets/images/logo.svg" alt = "CAD-EDU logo, link to homepage">
+</a >
+<section class="navpageTitle rounded">
+	<h1 class="PageTitle">Pages</h1>
+	<ul class="dropdown rounded">
+		<a href="../contact.html">
+			<h1 class="PageTitle">Contact</h1>
+		</a>
+		<hr>
+		<ul>
+			<a href="../articles.html">
+				<h1 class="PageTitle">Articles</h1>
+			</a>
+			<li>
+				<a href="../articles/3D_Printing.html">3D Printing</a>
+			</li>
+			<li>
+				<a href="../articles/CAD_Software.html">CAD Software</a>
+			</li>
+			<li>
+				<a href="../articles/CnC_Machining.html">CNC machines</a>
+			</li>
+			<li>
+				<a href="../articles/Factory_Lines.html">Factory Lines</a>
+			</li>
+			<li>
+				<a href="../articles/REPRAP.html">REPRAP</a>
+			</li>
+			<li>
+				<a href="../articles/Robotics.html">Robotics</a>
+			</li>
+		</ul>
+	</ul>
+</section>`;
