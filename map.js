bfec128826eb764a4f966b221c1f212cd52a3eac
Julien Peeters <julienpeeters.personal@gmail.com>
2022-12-04 01:07:56 +0100

    ''

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
-let map = L.map('map').setView([51.23009, 4.41616], 17);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+let map = L.map("map").setView([51.23009, 4.41616], 17);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
